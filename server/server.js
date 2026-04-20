8e7bb82e444c7e90b5a8b132790dc4a8f1a0ca43
Thanisha G <thanisha293@gmail.com>
2026-04-20 16:24:09 +0530

    Server page changes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import fs from "fs";
 import connectDB from "./config/db.js";
 import backupRoutes from "./routes/backupRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import adminroutes from "./routes/adminroutes.js";
-import "./utils/cleanupJob.js";
+import Backup from "./models/Backup.js";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
+
+// Fix __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // DB
 connectDB();
@@ -18,61 +25,15 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-// ✅ Only expose verified files
-app.use("/uploads/backup", express.static("uploads/backup"));
+// ✅ FIXED STATIC (IMPORTANT)
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
 app.use("/api/files", backupRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminroutes);
 
-
 // Test route
 app.get("/", (req, res) => {
   res.send("API running");
 });
-
-// File size error handling
-app.use((err, req, res, next) => {
-  if (err.code === "LIMIT_FILE_SIZE") {
-    return res.status(400).json({ message: "File too large. Max 10MB allowed." });
-  }
-  next(err);
-});
-
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-// delete after two days in admin 
-
-setInterval(async () => {
-  try {
-    const now = new Date();
-
-    const files = await Backup.find({
-      isDeleted: true,
-      deletedAt: { $ne: null }
-    });
-
-    for (let file of files) {
-      const diff = (now - file.deletedAt) / (1000 * 60 * 60 * 24);
-
-      if (diff >= 2) { // 🔥 2 days
-        const filePath = path.join(process.cwd(), file.filePath);
-
-        if (fs.existsSync(filePath)) {
-          fs.unlinkSync(filePath);
-        }
-
-        await Backup.findByIdAndDelete(file._id);
-
-        console.log("Auto deleted:", file.fileName);
-      }
-    }
-
-  } catch (err) {
-    console.log("Auto delete error:", err);
-  }
-}, 60 * 60 * 1000); // runs every 1 hour
